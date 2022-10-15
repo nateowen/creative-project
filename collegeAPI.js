b86e0e34875ccd8282164288d486967082848f2e
Nathan Owen <nathanrheadowen@gmail.com>
2022-10-15 16:43:29 -0600

    Finished fetching data and displaying schools, now just need to fix styling of search bar.

--- collegeAPI.js
+++ collegeAPI.js
@@ -4,15 +4,27 @@ document.getElementById("schoolSubmit").addEventListener("click", function(event
     if (value === "")
         return;
         
-    const url = "https://api.data.gov/ed/collegescorecard/v1/schools?api_key=kVx3ziG0HyZi9JC5MuhKQ6dkGVEE2AhiajjrzzzT&fields=id,school.name,school.alias,school.search&per_page=20&sort=school.alias:asc&school.search=" + value;
+    const url = "https://api.data.gov/ed/collegescorecard/v1/schools?api_key=kVx3ziG0HyZi9JC5MuhKQ6dkGVEE2AhiajjrzzzT&fields=id,school.name,school.city,latest.cost.attendance.academic_year,latest.student.size,latest.admissions.admission_rate.overall,latest.admissions.act_scores.midpoint.cumulative&per_page=20&sort=latest.student.size:desc&school.search=" + value;
     fetch(url)
-      .then(function(response) {
-        return response.json();
-      }).then(function(json) {
-        console.log(json)
+    .then((response) => response.json())
+    .then((data) => {
         let results = "";
-            
+        
+        for (var i = 0; i < data.results.length; i++) {
+          console.log(data.results[i]);
+          results += '<div class="school col w-100 text-dark bg-light rounded p-md-5 m-5 text-center overflow-hidden">'
+          results += '<h2>' + data.results[i]['school.name'] + '</h2>';
+          results += '<p>' + data.results[i]['school.city'] + '</p>';
+
+          results += '<h5>Total Student Enrollment: ' + Math.round(data.results[i]['latest.student.size'] / 1000) + ',000</h5>';
+          results += '<h5>Tutition: $' + Math.round(data.results[i]['latest.cost.attendance.academic_year'] / 1000) + 'k</h5>';
+          results += '<h5>Admission Rate: ' + ((data.results[i]['latest.admissions.admission_rate.overall'])*100).toFixed(2) + '%</h5>';
+
+          results += '</div>'
+        }
+
+        document.getElementById('schoolSquare').innerHTML = results;
         //begin adding to results to show school data
         
-      });
+      })
 });
